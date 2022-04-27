fc800263ae9aa3b7b3ca5ad3ffa88f6f2590838c
codesirohi <siddhantshubham23@gmail.com>
2022-04-27 18:01:56 +0530

    test run

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-dotenv.config();
+ //dotenv.config();
 
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send(`<h2> Working </h2>`);
+  res.send(`<h2> Working </h2> \n`);
+  
 });
 
 app.listen(port, () => {
   console.log(`[server]: server is running at https:localhost:${port}`);
 });
+
